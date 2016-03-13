47b1c115dabe57676f28bc649777cb6db376155a
manidf <mannuel.ferreira@gmail.com>
2016-03-13 21:48:24 +0200

    update

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 class App extends React.Component {
-	render () {
-		return <div>Hello</div>
+	render() {
+			return (
+				<div>
+						<h1> Hello </h1>
+				</div>
+		);
 	}
 }
 
