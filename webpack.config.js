5efc9eff9c49eb54b09fa26d269e302f4f24fbb8
manidf <mannuel.ferreira@gmail.com>
2016-07-30 16:32:18 +0200

    babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-	entry: './index.js',
+	entry: './src/js/index.js',
 	output: {
-		path: './public/',
-		filename: 'app.js'
+		path: './dist/',
+		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	devServer: {
 		inline: true,
+		contentBase: './dist',
 		port: 3080
 	},
 	module: {
 		loaders: [
 			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				loader: 'babel',
+				test: /(\.jsx$|\.js$)/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
 				query: {
-					presets: ['es2015', 'react']
+					presets: ['react', 'es2015']
 				}
 			}
 		]
