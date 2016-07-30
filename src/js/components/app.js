3a49c2db1fa3f7a6f0109ea47e0d87ebaf11a23d
manidf <mannuel.ferreira@gmail.com>
2016-07-30 18:06:32 +0200

    added actions and contants

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import AppActions from '../actions/app-actions';
 
 export default class App extends React.Component {
     render() {
-        return <h3> React, Flux Architecture</h3>;
+        return <h3
+        onClick={AppActions.addItem.bind(null, 'this is an item')}
+        > React, Flux Architecture</h3>;
     }
 }
--- src/js/dispatchers/app-dispatcher.js
+++ src/js/dispatchers/app-dispatcher.js
@@ -6,6 +6,7 @@ export function register( callback ) {
     return flux.register( callback );
 }
 
-export function dispatch( actionType, action ) { 
+export function dispatch( actionType, action ) {
+    console.log(actionType);
     flux.dispatch( actionType, action );
 }
