825835e0f8486b38adeacee9822a749e725a4b80
manidf <mannuel.ferreira@gmail.com>
2016-07-30 16:49:41 +0200

    dispatcher added

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
-        return <h1> Hello</h1>;
+        return <h3> React, Flux Architecture</h3>;
     }
 }
